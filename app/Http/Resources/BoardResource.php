ea05e2ad64d18da3df6fc2eec37422324eefa7b6
Juan Felipe Rivera G <jjuanrivvera@gmail.com>
2024-02-09 21:54:26 -0500

    Fix typo

--- app/Http/Resources/BoardResource.php
+++ app/Http/Resources/BoardResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class BoardResourse extends JsonResource
+class BoardResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
